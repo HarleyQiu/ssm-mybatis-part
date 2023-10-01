622aa7347b12e3baf15df0cfda6056145b0252e6
QiuLinhao <210554114@qq.com>
2023-10-01 14:56:12 +0800

    动态SQL set trim choose(when otherwise) foreach标签

---
+++ mybatis-high-dynsql-06/src/test/java/org/example/test/MyBatisTest.java
@@ -0,0 +1,45 @@
+package org.example.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.example.mapper.EmployeeMapper;
+import org.example.pojo.Employee;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+public class MyBatisTest {
+
+    private SqlSession session;
+
+    // junit会在每一个@Test方法前执行@BeforeEach方法
+    @BeforeEach
+    public void init() throws IOException {
+        session = new SqlSessionFactoryBuilder()
+                .build(
+                        Resources.getResourceAsStream("mybatis-config.xml"))
+                .openSession();
+    }
+
+    //测试if where标签
+    @Test
+    public void test_01() {
+        EmployeeMapper mapper = session.getMapper(EmployeeMapper.class);
+        List<Employee> list = mapper.queryChoose("tom", null);
+        System.out.println("list = " + list);
+    }
+
+
+    // junit会在每一个@Test方法后执行@@AfterEach方法
+    @AfterEach
+    public void clear() {
+        session.commit();
+        session.close();
+    }
+}
